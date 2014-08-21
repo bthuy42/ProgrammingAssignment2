76abc44ea53862796f4fcc5f5e954b4e5869b68d
The Bee <bthuy.nguyen42@gmail.com>
2014-08-20 21:24:02 -0500

    initial commit
    Testing commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this 
+## The makeCacheMatrix function will do the following:
+## --  Create a special "matrix"
+## --  Return cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse.matrix <- diag(nrow(x)) %/% x
+    
+    ## print(diag(dim(x)))
+    print(paste("x is: ", x))
+    print(paste("invers of x is:  ", inverse.matrix))
 }
 
 
